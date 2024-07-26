f2576eaa500c95e92161c720ea1f5f12fef23232
Daniel Williams <williamsoligodaniel@gmail.com>
2024-07-26 18:23:23 +0100

    Create lab.js

---
+++ module 2/variables/lab.js
@@ -0,0 +1,30 @@
+// Estimated time
+// 15-30 minutes
+
+// Level of difficulty
+// Easy
+
+// Objectives
+// Familiarize the student with:
+
+// variables (i.e. naming, declaring, initializing and modifying their values)
+// Scenario
+// Our task will be to create a list of contacts. Initially, the list will be quite simple: we will only write three people to it using the data shown in the table below.
+// In the rest of the course, you will return to this script and systematically extend it with new functionality, using the newly learned elements of JavaScript.
+
+// Declare and initialize the variables where you will store all the information (nine variables in total). Display in the console information about the first and last contact in the form: name/phone/email.
+
+let maxName = "Maxwell Wright";
+let maxNum = "(091)7196495";
+let maxMail = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let rajName = "Raja Villarreal";
+let rajNum = "0866982895";
+let rajMail = "posuere.vulputate@sed.com";
+
+let helenName = "Helen Richichards";
+let helenNum = "080111";
+let helenMail = "libero@convallis.edu";
+
+console.log(maxName + "/" + maxNum + "/" + maxMail);
+console.log(helenName + "/" + helenNum + "/" + helenMail);
