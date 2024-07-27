102b9bf9e48e38801e3217e438b2a6668511eb10
Daniel Williams <williamsoligodaniel@gmail.com>
2024-07-27 15:52:12 +0100

    Create lab3.js

---
+++ module 2/variables/lab3.js
@@ -0,0 +1,7 @@
+// Question 2: Declare an empty object and save it to a person variable. Using dot notation, add the name and surname fields to the object by entering your data as values. Try to display the individual fields on the console.
+
+const person = {};
+person.name = 'Dan';
+person.surname = 'Will';
+console.log(person)
+console.log(`${person.name} ${person.surname}`);
